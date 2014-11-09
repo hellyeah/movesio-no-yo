ab99ccd9b5654770b6ab612f9c8415be2799e272
gailees <gailees@umich.edu>
2014-11-08 23:01:29 -0800

    took over new tab

--- MyCloudCode/cloud/main.js
+++ MyCloudCode/cloud/main.js
@@ -1,11 +1,30 @@
-
+var _ = require('underscore');
 // Use Parse.Cloud.define to define as many cloud functions as you want.
 // For example:
 Parse.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
 
+var validateEmail = function(email, callback) {
+    Parse.Cloud.httpRequest({
+        url: 'https://api.mailgun.net/v2/address/validate',
+        params: {
+            address: email,
+            api_key: 'pubkey-70yfqdibeeabn9oa7sc-nmtyo5gfi9z4'
+        },
+      success: function(httpResponse) {
+        console.log(httpResponse.text);
+        response.success(httpResponse.text);
+      },
+      error: function(httpResponse) {
+        console.error('Request failed with response code ' + httpResponse.status);
+        response.success('Request failed with response code ' + httpResponse.status);
+      }
+    });
+};
+
 Parse.Cloud.define("validateEmail", function(request, response) {
+    //response.success(validateEmail(request.params.email));
     Parse.Cloud.httpRequest({
         url: 'https://api.mailgun.net/v2/address/validate',
         params: {
@@ -18,7 +37,7 @@ Parse.Cloud.define("validateEmail", function(request, response) {
       },
       error: function(httpResponse) {
         console.error('Request failed with response code ' + httpResponse.status);
-        response.error('Request failed with response code ' + httpResponse.status);
+        response.success('Request failed with response code ' + httpResponse.status);
       }
     });
 });
@@ -39,13 +58,44 @@ Parse.Cloud.define("getFoundersEmail", function(request, response) {
             Parse.Cloud.httpRequest({
                 url: crunchbaseUrl,
                 params: {
-                    user_key : 'e8be743a88105142592646e82ea8fd76',
+                    user_key : '06f39bc64703ba0a493eff6945e0200f',
                 },
               success: function(httpResponse) {
-                var foundersName = JSON.parse(httpResponse.text).data.relationships.founders.items[0].name;
+                //httpResponse has the entire organization so I can see all the employees, team, etc
+                //could look for CEO?
+                var people = JSON.parse(httpResponse.text).data.relationships;
+                //
+                var founder;
+                var foundersName;
+                if (people.founders) {
+                    foundersName = people.founders.items[0].name;
+                }
+                else if (_.find(people.current_team.items, function(person){ return person.title === "CEO"; })){
+                    var ceo = _.find(people.current_team.items, function(person){ return person.title === "CEO"; });
+                    foundersName = ceo.first_name + " " + ceo.last_name;
+                }
+                else {
+                    var person = people.current_team.items[0];
+                    foundersName = person.first_name + " " + person.last_name;
+                }
+
+                //var founder = (_.find(people.current_team.items, function(person){ return person.title === "CEO"; })) || people.founders.items[0]; 
+                //console.log()
+                //var foundersName = founder.name;
                 var foundersFirstName = foundersName.substr(0,foundersName.indexOf(' '));
                 var foundersEmail = foundersFirstName+"@"+request.params.url;
+                //run all combos
                 //Validate Email
+                // validateEmail(foundersEmail, function(blah) {
+                //     success: function(blah) {
+                //         response.success(foundersEmail);
+                //     },
+                //     error: function(blah) {
+                //         console.error('Request failed with response code ' + httpResponse.status);
+                //         response.error('Request failed with response code ' + httpResponse.status);                        
+                //     }
+                // })
+
                 Parse.Cloud.httpRequest({
                     url: 'https://api.mailgun.net/v2/address/validate',
                     params: {
@@ -58,6 +108,7 @@ Parse.Cloud.define("getFoundersEmail", function(request, response) {
                     response.success(foundersEmail.toLowerCase());
                   },
                   error: function(httpResponse) {
+                    //if founders email isnt found, do this
                     console.error('Request failed with response code ' + httpResponse.status);
                     response.error('Request failed with response code ' + httpResponse.status);
                   }
